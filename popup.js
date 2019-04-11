da7d9cf8e4720d71d1c7d1b47ff177c9139ff54d
Ken Ronoh <kennethronoh1@gmail.com>
2019-04-12 00:43:02 +0300

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,8 @@
+<script>
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+</script>
